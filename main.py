0f8c878870fb780844c5cad44c22053766291527
sujan bose <112420725+sujanboseb@users.noreply.github.com>
2024-10-18 16:23:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -182,16 +182,17 @@ def handle_message():
                     # Proceed to ask for meeting_id if not already provided
                     return meeting_cancelling_id(phone_number, meeting_booking_id)
 
-            if intent == 'Greetings':
-              # Respond with the message for meeting and cab management
-              greeting_message = (
-                  "1.This number is for meeting and cab management. "
-                  "2.You can check  your meetings from the past dates. "
-                  "3.Please provide the **meeting date** in **'dd/mm/yyyy'** format and the ** time **  in **'hh:mm AM/PM'** format."
-                  "4.if the text has been **STOP** means then u can satrt new conversation ok "
-                  "5. cab booking time can be around **6:30pm** or **7:30pm** only "
-              )
-              return jsonify(greeting_message)
+           if intent == 'Greetings':
+                # Respond with the message for meeting and cab management
+                greeting_message = (
+                    "1. This number is for meeting and cab management. `"
+                    "2. You can check your meetings from the past dates. `"
+                    "3. Please provide the **meeting date** in **'dd/mm/yyyy'** format and the **time** in **'hh:mm AM/PM'** format. `"
+                    "4. If the text has been **STOP** means then you can start a new conversation. `"
+                    "5. Cab booking time can be around **6:30pm** or **7:30pm** only. `"
+                )
+                return jsonify(greeting_message)
+
 
             elif intent == 'cab_cancelling':
                 # Check if meeting_id is provided
